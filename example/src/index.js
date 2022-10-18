f649189dd653cd70ea6bfc991362074fcef8f9e0
karis courey <kariscourey@gmail.com>
2022-10-18 15:36:48 -0400

    update

---
+++ example/src/index.js
@@ -0,0 +1,41 @@
+import { createRoot } from 'react-dom/client';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import App from './App';
+import Expenses from './Expenses';
+import Invoices from './Invoices';
+import Invoice from './Invoice';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}>
+        <Route path='expenses' element={<Expenses />} />
+        <Route path='invoices' element={<Invoices />}>
+          <Route index
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>Select an invoice</p>
+              </main>
+            }
+          />
+          <Route path=':invoiceId' element={<Invoice />} />
+        </Route>
+        <Route
+          path='*'
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
